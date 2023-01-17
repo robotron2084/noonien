c97588b27ee364352035d47339054672655c1108
Chris Hill <chris@enemyhideout.com>
2023-01-16 20:06:47 -0700

    Improving delegation of display properties in inspector.

--- Editor/EntitySourceInspector.cs
+++ Editor/EntitySourceInspector.cs
@@ -45,6 +45,69 @@ namespace com.enemyhideout.soong.editor
       }
     }
 
+    private static Dictionary<Type, Action<PropertyInfo, DataElement>> _propertyDelegates = new Dictionary<Type, Action<PropertyInfo, DataElement>>()
+    {
+      {typeof(bool),  BoolProperty},
+      {typeof(string),  StringProperty},
+      {typeof(int),  IntProperty},
+      {typeof(float),  FloatProperty},
+      {typeof(Vector2),  Vector2Property},
+      {typeof(Vector3),  Vector3Property},
+      {typeof(Vector4),  Vector4Property},
+      {typeof(Rect),  RectProperty},
+    };
+
+    private static void ShowProperty<T>(PropertyInfo propertyInfo, DataElement element, Func<T,T> showEditor)
+    {
+      EditorGUI.BeginChangeCheck();
+      T value = (T)propertyInfo.GetValue(element);
+      T newValue = showEditor(value);
+      if (EditorGUI.EndChangeCheck())
+      {
+        propertyInfo.SetValue(element, newValue);
+      }
+    }
+    
+    private static void StringProperty(PropertyInfo propertyInfo, DataElement element)
+    {
+      ShowProperty<string>(propertyInfo, element, (x) => EditorGUILayout.TextField(propertyInfo.Name, x));
+    }
+
+    private static void Vector2Property(PropertyInfo propertyInfo, DataElement element)
+    {
+      ShowProperty<Vector2>(propertyInfo, element, (x) => EditorGUILayout.Vector2Field(propertyInfo.Name, x));
+    }
+
+    private static void Vector3Property(PropertyInfo propertyInfo, DataElement element)
+    {
+      ShowProperty<Vector3>(propertyInfo, element, (x) => EditorGUILayout.Vector3Field(propertyInfo.Name, x));
+    }
+    
+    private static void Vector4Property(PropertyInfo propertyInfo, DataElement element)
+    {
+      ShowProperty<Vector4>(propertyInfo, element, (x) => EditorGUILayout.Vector4Field(propertyInfo.Name, x));
+    }
+
+    private static void RectProperty(PropertyInfo propertyInfo, DataElement element)
+    {
+      ShowProperty<Rect>(propertyInfo, element, (x) => EditorGUILayout.RectField(propertyInfo.Name, x));
+    }
+
+    private static void IntProperty(PropertyInfo propertyInfo, DataElement element)
+    {
+      ShowProperty<int>(propertyInfo, element, (x) => EditorGUILayout.IntField(propertyInfo.Name, x));
+    }
+
+    private static void FloatProperty(PropertyInfo propertyInfo, DataElement element)
+    {
+      ShowProperty<float>(propertyInfo, element, (x) => EditorGUILayout.FloatField(propertyInfo.Name, x));
+    }
+
+    private static void BoolProperty(PropertyInfo propertyInfo, DataElement element)
+    {
+      ShowProperty<bool>(propertyInfo, element, (x) => EditorGUILayout.Toggle(propertyInfo.Name, x));
+    }
+
     private static void EditorForElement(DataElement element)
     {
       
@@ -66,24 +129,14 @@ namespace com.enemyhideout.soong.editor
           {
             if (propertyInfo.CanWrite)
             {
-              TypeCode typeCode = Type.GetTypeCode(propertyInfo.PropertyType);
-              EditorGUI.BeginChangeCheck();
-              switch (typeCode)
+              if (_propertyDelegates.TryGetValue(propertyInfo.PropertyType, out var del))
               {
-                case TypeCode.Boolean:
-                  bool value = (bool)propertyInfo.GetValue(element);
-                  bool newValue = EditorGUILayout.Toggle(propertyInfo.Name,value);
-                  if (EditorGUI.EndChangeCheck())
-                  {
-                    propertyInfo.SetValue(element, newValue);
-                  }
-                  break;
-              
-                default:
-                  // just show it.
-                  object val = propertyInfo.GetValue(element);
-                  EditorGUILayout.LabelField($"{propertyInfo.Name} : '{val}'");
-                  break;
+                del(propertyInfo, element);
+              }
+              else
+              {
+                object val = propertyInfo.GetValue(element);
+                EditorGUILayout.LabelField($"{propertyInfo.Name} : '{val}'");
               }
             }
           }
